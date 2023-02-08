b4097a3b75200eabb93084cf8c1ca5023abff81c
kevl0317 <kevin.liu1270@gmail.com>
2023-02-07 21:23:03 -0800

    UI Design First Draft

--- app/src/main/java/edu/ucsd/cse110/cse110_team17_project/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_team17_project/MainActivity.java
@@ -2,7 +2,10 @@ package edu.ucsd.cse110.cse110_team17_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
