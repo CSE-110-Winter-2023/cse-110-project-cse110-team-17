a2f7c1112f07c4a0959042606174959023ee99cc
njmorales <njmorales@ucsd.edu>
2023-02-09 17:47:27 -0800

    save inputs on submit and load inputs on start

--- app/src/main/java/edu/ucsd/cse110/cse110_team17_project/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_team17_project/MainActivity.java
@@ -2,6 +2,7 @@ package edu.ucsd.cse110.cse110_team17_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +13,16 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadInputs();
+    }
+
+    @Override
+    protected void onDestroy() {
+        saveInputs();
+        super.onDestroy();
     }
 
     public void onSubmitbtnClicked(View view) throws Exception {
@@ -36,5 +43,55 @@ public class MainActivity extends AppCompatActivity {
             coordinate_2.setError("Missing Coordinates!");
             coordinate_3.setError("Missing Coordinates!");
         }
+        finish();
+    }
+
+    public void loadInputs() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        String label1 = preferences.getString("label1", "");
+        String label2 = preferences.getString("label2", "");
+        String label3 = preferences.getString("label3", "");
+
+        String coordinate1 = preferences.getString("coordinate1", "");
+        String coordinate2 = preferences.getString("coordinate2", "");
+        String coordinate3 = preferences.getString("coordinate3", "");
+
+        EditText labelField1 = findViewById(R.id.label_name1);
+        EditText labelField2 = findViewById(R.id.label_name2);
+        EditText labelField3 = findViewById(R.id.label_name3);
+
+        EditText coordinateField1 = findViewById(R.id.coordinate1);
+        EditText coordinateField2 = findViewById(R.id.coordinate2);
+        EditText coordinateField3 = findViewById(R.id.coordinate3);
+
+        labelField1.setText(label1);
+        labelField2.setText(label2);
+        labelField3.setText(label3);
+
+        coordinateField1.setText(coordinate1);
+        coordinateField2.setText(coordinate2);
+        coordinateField3.setText(coordinate3);
+    }
+
+    public void saveInputs() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        EditText labelField1 = findViewById(R.id.label_name1);
+        EditText labelField2 = findViewById(R.id.label_name2);
+        EditText labelField3 = findViewById(R.id.label_name3);
+
+        EditText coordinateField1 = findViewById(R.id.coordinate1);
+        EditText coordinateField2 = findViewById(R.id.coordinate2);
+        EditText coordinateField3 = findViewById(R.id.coordinate3);
+
+        editor.putString("label1", labelField1.getText().toString());
+        editor.putString("label2", labelField2.getText().toString());
+        editor.putString("label3", labelField3.getText().toString());
+        editor.putString("coordinate1", coordinateField1.getText().toString());
+        editor.putString("coordinate2", coordinateField2.getText().toString());
+        editor.putString("coordinate3", coordinateField3.getText().toString());
+
+        editor.apply();
     }
 }
