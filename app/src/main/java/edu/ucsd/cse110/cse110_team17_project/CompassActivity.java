b530f8efecefac4238658dcbcbe95a029a346fa3
ziguan437 <ziguan@ucsd.edu>
2023-02-13 20:08:27 -0800

    Update rotation

--- app/src/main/java/edu/ucsd/cse110/cse110_team17_project/CompassActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_team17_project/CompassActivity.java
@@ -1,7 +1,9 @@
 package edu.ucsd.cse110.cse110_team17_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,6 +23,7 @@ public class CompassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         Bundle coordInfo = getIntent().getExtras();
         TextView name_label1 = (TextView) findViewById(R.id.label_1);
@@ -40,15 +43,29 @@ public class CompassActivity extends AppCompatActivity {
 
     public void onRotateTestClicked(View view) {
         ImageView compass = (ImageView) findViewById(R.id.compass_face);
-
+        TextView label_1 = (TextView) findViewById(R.id.label_1);
+        TextView label_2 = (TextView) findViewById(R.id.label_2);
+        TextView label_3 = (TextView) findViewById(R.id.label_3);
+        ConstraintLayout.LayoutParams layoutParams1 = (ConstraintLayout.LayoutParams) label_1.getLayoutParams();
+        ConstraintLayout.LayoutParams layoutParams2 = (ConstraintLayout.LayoutParams) label_2.getLayoutParams();
+        ConstraintLayout.LayoutParams layoutParams3 = (ConstraintLayout.LayoutParams) label_3.getLayoutParams();
 
         this.future = backgroundExecutor.submit(() -> {
             float angle1 = 0;
             do {
 
                 final float finalAngle = angle1;
+                layoutParams1.circleAngle = finalAngle;
+                layoutParams2.circleAngle = finalAngle + 90;
+                layoutParams3.circleAngle = finalAngle + 180;
                 runOnUiThread(() -> {
                     compass.setRotation(finalAngle);
+                    label_1.setRotation(finalAngle);
+                    label_2.setRotation(finalAngle + 90);
+                    label_3.setRotation(finalAngle + 180);
+                    label_1.setLayoutParams(layoutParams1);
+                    label_2.setLayoutParams(layoutParams2);
+                    label_3.setLayoutParams(layoutParams3);
                 });
                 angle1 += 1;
 
@@ -64,7 +81,22 @@ public class CompassActivity extends AppCompatActivity {
     public void onAngleConfirmClicked(View view) {
         TextView AngleInput = (TextView) findViewById(R.id.input_angle);
         ImageView compass = (ImageView) findViewById(R.id.compass_face);
+        TextView label_1 = (TextView) findViewById(R.id.label_1);
+        TextView label_2 = (TextView) findViewById(R.id.label_2);
+        TextView label_3 = (TextView) findViewById(R.id.label_3);
+        ConstraintLayout.LayoutParams layoutParams1 = (ConstraintLayout.LayoutParams) label_1.getLayoutParams();
+        ConstraintLayout.LayoutParams layoutParams2 = (ConstraintLayout.LayoutParams) label_2.getLayoutParams();
+        ConstraintLayout.LayoutParams layoutParams3 = (ConstraintLayout.LayoutParams) label_3.getLayoutParams();
+
         Integer angle = Integer.parseInt(AngleInput.getText().toString());
         compass.setRotation(angle.floatValue());
+
+        label_1.setRotation(angle);
+        label_2.setRotation(angle + 90);
+        label_3.setRotation(angle + 180);
+        label_1.setLayoutParams(layoutParams1);
+        label_2.setLayoutParams(layoutParams2);
+        label_3.setLayoutParams(layoutParams3);
+
     }
 }
