1d628390f28aec0d6966d561fa03027d154dc242
leo3friedman <73138942+leo3friedman@users.noreply.github.com>
2023-02-19 19:29:24 -0700

    use user location to display labels

---
+++ app/src/main/java/edu/ucsd/cse110/cse110_team17_project/LocationService.java
@@ -0,0 +1,62 @@
+package edu.ucsd.cse110.cse110_team17_project;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Pair;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class LocationService implements LocationListener {
+    private static LocationService instance;
+    private Activity activity;
+
+    private MutableLiveData<Pair<Double, Double>> locationValue;
+
+    private final LocationManager locationManager;
+
+    public static LocationService singleton(Activity activity) {
+        if(instance ==null){
+            instance = new LocationService(activity);
+        }
+        return instance;
+    }
+    protected LocationService(Activity activity){
+        this.locationValue = new MutableLiveData<>();
+        this.activity = activity;
+        this.locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        this.registerLocationListener();
+    }
+
+    private void registerLocationListener(){
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            throw new IllegalStateException("App needs location permission to get latest location");
+        }
+        this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+
+    public void onLocationChanged(@NonNull Location location){
+        this.locationValue.postValue(new Pair<Double, Double>(location.getLatitude(), location.getLongitude()));
+    }
+
+    private void unregisterLocationListener(){
+        locationManager.removeUpdates(this);
+    }
+    public LiveData<Pair<Double, Double>> getLocation(){return this.locationValue;}
+
+    public void setMockOrientationSource(MutableLiveData<Pair<Double, Double>> mockDataSource) {
+        unregisterLocationListener();
+        this.locationValue = mockDataSource;
+    }
+}
+
+
--- app/src/main/java/edu/ucsd/cse110/cse110_team17_project/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_team17_project/MainActivity.java
@@ -2,10 +2,14 @@ package edu.ucsd.cse110.cse110_team17_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
@@ -18,19 +22,24 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-    private double lat;
-    private double lon;
 
     public OrientationService orientationService;
-    private boolean isSensorActivated = false;
+//    private boolean isSensorActivated = false;
 
     // Right now these are arbitrary values, when we got 3-1 done we should have specific values for these
-    private Float cord1Angle = null;
-    private Float cord2Angle = null;
-    private Float cord3Angle = null;
+//    private Float cord1Angle = null;
+//    private Float cord2Angle = null;
+//    private Float cord3Angle = null;
     private float compassNorthAngle = 0;
 
-    private Pair<Double, Double> currentLocation = new Pair<>(32.715736,-117.161087);
+    private LocationService locationService;
+
+    private Pair<Double, Double> coordinate1;
+    private Pair<Double, Double> coordinate2;
+    private Pair<Double, Double> coordinate3;
+
+    // defaults to San Diego (fix that later)
+    private Pair<Double, Double> currentLocation = new Pair<>(32.715736, -117.161087);
 
 
     @Override
@@ -38,35 +47,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+        }
+        locationService = LocationService.singleton(this);
+
+        locationService.getLocation().observe(this, loc->{
+            System.out.println(Double.toString(loc.first) + ", " + Double.toString(loc.second));
+            currentLocation = loc;
+        });
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         System.out.println("resumed");
-        // Sensor is deactivated when you navigate to new Activity (it need to be reactivated on resume)
-        isSensorActivated = false;
-
 
         // Get all the label names and coordinates from the SharedPreference object
         SharedPreferences preferences = getSharedPreferences("MAIN", MODE_PRIVATE);
         String label1 = preferences.getString("label1", "");
         String label2 = preferences.getString("label2", "");
         String label3 = preferences.getString("label3", "");
-        Pair<Double, Double> coordinate1 = Utilities.validCoordinate(preferences.getString("coordinate1", ""));
-        Pair<Double, Double> coordinate2 = Utilities.validCoordinate(preferences.getString("coordinate2", ""));
-        Pair<Double, Double> coordinate3 = Utilities.validCoordinate(preferences.getString("coordinate3", ""));
-
-        // Set Coordinate Angles
-        if(coordinate1 != null){
-            cord1Angle = (float) Utilities.updateAngle(currentLocation.first.floatValue(), currentLocation.second.floatValue(), coordinate1.first.floatValue(), coordinate1.second.floatValue());
-        }
-        if(coordinate2 != null) {
-            cord2Angle = (float) Utilities.updateAngle(currentLocation.first.floatValue(), currentLocation.second.floatValue(), coordinate2.first.floatValue(), coordinate2.second.floatValue());
-        }
-        if(coordinate3 != null) {
-            cord3Angle = (float) Utilities.updateAngle(currentLocation.first.floatValue(), currentLocation.second.floatValue(), coordinate3.first.floatValue(), coordinate3.second.floatValue());
-        }
+        coordinate1 = Utilities.validCoordinate(preferences.getString("coordinate1", ""));
+        coordinate2 = Utilities.validCoordinate(preferences.getString("coordinate2", ""));
+        coordinate3 = Utilities.validCoordinate(preferences.getString("coordinate3", ""));
 
 
         // Check if all of them is empty, if yes, we have no input yet and need to go to InputActivity
@@ -77,8 +83,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         orientationService = OrientationService.singleton(this);
-
-        // Sets
         startOrientationSensor(orientationService);
 
 
@@ -105,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(inputIntent);
     }
 
-    public void onClearClicked(View view){
+    public void onClearClicked(View view) {
         SharedPreferences preferences = getSharedPreferences("MAIN", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.clear();
@@ -116,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
     public void onAngleConfirmClicked(View view) {
         // Stop the sensors first
         orientationService.unregisterSensorListeners();
-        isSensorActivated = false;
+//        isSensorActivated = false;
 
         TextView AngleInput = (TextView) findViewById(R.id.input_angle);
         Integer angle = Integer.parseInt(AngleInput.getText().toString());
@@ -127,22 +131,21 @@ public class MainActivity extends AppCompatActivity {
     // This method starts the orientation sensor
     private void startOrientationSensor(OrientationService orientationService) {
         // If sensor is already activated, no need to start again
-        if (!isSensorActivated) {
-            orientationService.registerSensorListeners();
-            isSensorActivated = true;
+//        if (!isSensorActivated) {
+        orientationService.registerSensorListeners();
+//            isSensorActivated = true;
 
-            orientationService.getOrientation().observe(this, orientation -> {
-                float actualAngle = -orientation / (float) Math.PI * 180;
-                setAllLabelRotations(actualAngle);
-            });
+        orientationService.getOrientation().observe(this, orientation -> {
+            float actualAngle = -orientation / (float) Math.PI * 180;
+            setAllLabelRotations(actualAngle);
+        });
 
 
-        }
+//        }
     }
 
     // If we want to resume sensors, we clicked on the "Resume sensors" Button and start the orientation again
     public void onResumeSensorsClicked(View view) {
-        System.out.println("Resumed Sensors");
         orientationService = new OrientationService(this);
         startOrientationSensor(orientationService);
     }
@@ -159,19 +162,22 @@ public class MainActivity extends AppCompatActivity {
 
         compass.setRotation(angle + compassNorthAngle);
 
-        if (cord1Angle != null){
-            name_label1.setRotation(angle + cord1Angle);
-            layoutParams1.circleAngle = angle + cord1Angle;
+        if (coordinate1 != null) {
+           float coordinate1Angle = (float) Utilities.updateAngle(currentLocation.first.floatValue(), currentLocation.second.floatValue(), coordinate1.first.floatValue(), coordinate1.second.floatValue());
+            name_label1.setRotation(angle + coordinate1Angle);
+            layoutParams1.circleAngle = angle + coordinate1Angle;
             name_label1.setLayoutParams(layoutParams1);
         }
-        if (cord2Angle != null){
-            name_label2.setRotation(angle + cord2Angle);
-            layoutParams2.circleAngle = angle + cord2Angle;
+        if (coordinate2 != null) {
+            float coordinate2Angle = (float) Utilities.updateAngle(currentLocation.first.floatValue(), currentLocation.second.floatValue(), coordinate2.first.floatValue(), coordinate2.second.floatValue());
+            name_label2.setRotation(angle + coordinate2Angle);
+            layoutParams2.circleAngle = angle + coordinate2Angle;
             name_label2.setLayoutParams(layoutParams2);
         }
-        if (cord3Angle != null){
-            name_label3.setRotation(angle + cord3Angle);
-            layoutParams3.circleAngle = angle + cord3Angle;
+        if (coordinate3 != null) {
+            float coordinate3Angle = (float) Utilities.updateAngle(currentLocation.first.floatValue(), currentLocation.second.floatValue(), coordinate3.first.floatValue(), coordinate3.second.floatValue());
+            name_label3.setRotation(angle + coordinate3Angle);
+            layoutParams3.circleAngle = angle + coordinate3Angle;
             name_label3.setLayoutParams(layoutParams3);
         }
     }
