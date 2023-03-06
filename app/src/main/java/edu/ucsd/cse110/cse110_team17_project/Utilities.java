0f74a63b2b60e5198aecbdf488f1639446cb815a
howardxu520 <xushuhao20015202@outlook.com>
2023-03-06 12:51:40 -0800

    add distance calculation function

--- app/src/main/java/edu/ucsd/cse110/cse110_team17_project/Utilities.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_team17_project/Utilities.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 import java.text.DecimalFormat;
 import java.util.Optional;
@@ -95,5 +97,24 @@ public class Utilities {
             return null;
         }
         return new Pair<>(latitude, longitude);
+    }
+
+    public static double distance(double latitude1, double longitude1,
+                                  double latitude2, double longitude2) {
+        latitude1 = Math.toRadians(latitude1);
+        longitude1 = Math.toRadians(longitude1);
+        latitude2 = Math.toRadians(latitude2);
+        longitude2 = Math.toRadians(longitude2);
+
+        double dlon = longitude2 - longitude1;
+        double dlat = latitude2 - latitude1;
+
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(latitude1) * Math.cos(latitude2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double r = 6371;
+        return(c * r);
     }
 }
