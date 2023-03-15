9c6ee2d58a9557d51f3d855a76844fd3de6ccc1e
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2023-03-14 19:56:30 -0700

    [no ci]

--- app/src/main/java/edu/ucsd/cse110/cse110_team17_project/viewmodel/ModelObserver.java
+++
@@ -1,5 +0,0 @@
-package edu.ucsd.cse110.cse110_team17_project.viewmodel;
-
-public interface ModelObserver {
-    void mUpdate();
-}
--- app/src/main/java/edu/ucsd/cse110/cse110_team17_project/viewmodel/Presenter.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_team17_project/viewmodel/Presenter.java
@@ -1,21 +1,45 @@
 package edu.ucsd.cse110.cse110_team17_project.viewmodel;
 
+import android.view.View;
+import android.widget.ImageView;
+
 import java.util.List;
 
-public class Presenter implements ModelObserver, ZoomObserver {
+public class Presenter implements ZoomObserver {
+    final float[] zoomSizes = new float[]{1F, 1.5F, 3F};
     int zoomPosition;
+    float zoomSize;
+    List<ImageView> circles;
     List<ZoomView> zoomViewList;
 
-    void presenter(int ZoomPosition){
+    void presenter(int ZoomPosition, List<ImageView> circles){
         this.zoomPosition = zoomPosition;
+        this.circles = circles;
     }
 
     @Override
     public void zoomUpdate(int zoomPosition) {
         this.zoomPosition = zoomPosition;
+        zoomSize = zoomSizes[zoomPosition];
+        for(int i = 0; i < circles.size(); i++){
+            ImageView circle = circles.get(i);
+            scaleCircle(circle);
+            visibleSet(circle, i);
+        }
     }
-    @Override
-    public void mUpdate() {
 
+
+
+    private void scaleCircle(ImageView circle){
+        circle.setScaleX(zoomSize);
+        circle.setScaleY(zoomSize);
+    }
+
+    private void visibleSet(ImageView circle, int i) {
+        if(i < zoomPosition){
+            circle.setVisibility(View.INVISIBLE);
+        }else{
+            circle.setVisibility(View.VISIBLE);
+        }
     }
 }
